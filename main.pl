66ed5809cec7e3158cde22e817d150d216d9bcac
Mark Garnett <thecommunistduck@hotmail.co.uk>
2015-03-23 19:44:38 +0000

    Fixed city generator, added drawing2

--- main.pl
+++ main.pl
@@ -1,10 +1,12 @@
 :- use_module(library(console)).
 :- use_module(library(color)).
 :- use_module(library(events)).
+:- use_module(library(mavis)).
 
 :- use_module(draw).
 :- use_module(input).
 :- use_module(utils).
+:- use_module(citygen).
 
 main(80, 60, 0).
 command(60, 40, 1).
@@ -26,6 +28,30 @@ run_loop(State):-
 run:-
 	init_main(State),
 	run_loop(State).
+
+test_city :-
+	generate_roads(area(0, 100, 0, 80), 15, 60, Areas, Roads),
+	init_root(100, 80, 'City Test', false),
+	create_console(city, 100,  80),
+	draw_roads(Roads).
+
+draw_roads([]) :-
+	blit(city, 0, 0, 0, 0, root, 0, 0),
+	flush.
+
+draw_roads([area(X1, X2, Y1, Y2)|Rs]) :-
+	color(lighter_green, Col1),
+	color(darker_green, Col2),
+	W is X2-X1,
+	H is Y2-Y1,
+	(
+		H = 1,
+		draw_line(city, h, X1, Y1, W, Col1)
+		;
+		draw_line(city, v, X1, Y1, H, Col2)
+	),
+	draw_roads(Rs),
+	input_main(_, _).
 
 go :-
 	console_size_full(main, W, H),
